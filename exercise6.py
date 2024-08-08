4bce8e60b8164c2b4592e49bc33ee6d3cce654b8
Marcem55 <malacalzamarcelo@gmail.com>
2024-08-07 21:53:05 -0300

    [ADD] exercise6, create a list of numbers from an input

---
+++ exercise6.py
@@ -0,0 +1,6 @@
+# Get a group of numbers separates by a "," and create a list
+
+num_entries = input("Enter numbers separated only by ',': ")
+print(num_entries)
+num_list = num_entries.split(",")
+print(num_list)
