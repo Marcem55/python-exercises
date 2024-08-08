9788a69e1b01b9a87a7c8564039c96f2986663e8
Marcem55 <malacalzamarcelo@gmail.com>
2024-08-07 21:25:27 -0300

    [ADD] exercise2, basic usage of print

---
+++ exercise2.py
@@ -0,0 +1,17 @@
+# Basic usage of print()
+
+print("Hello world!")
+print("This is a basic example ", end="---")
+print(" Let's go with Python!")
+print("Python", "is", "awesome!", sep="|")
+print("{} is {}".format("Python", "great!"))
+
+numbers = [2, 3, 5, 7, 11]
+print(numbers)
+
+capitals = {
+    "Colombia": "Bogotá",
+    "Argentina": "Buenos Aires",
+    "Perú": "Lima"
+}
+print(capitals)
