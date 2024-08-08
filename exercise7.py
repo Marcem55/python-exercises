99840c4dfca0dabcb4713782fd4ba465d5c26312
Marcem55 <malacalzamarcelo@gmail.com>
2024-08-07 21:56:16 -0300

    [ADD] exercise7, get the file extension from string

---
+++ exercise7.py
@@ -0,0 +1,5 @@
+# Get the file extension specified by the user
+
+file_name = input("Enter the file name: ")
+separate_file = file_name.split(".")
+print("The file extension is: {}".format(separate_file[1]))
