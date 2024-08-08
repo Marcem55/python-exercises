0d497ee3f8379a1e24034bf7f7d46cb57c789694
Marcem55 <malacalzamarcelo@gmail.com>
2024-08-07 22:01:10 -0300

    [ADD] exercise8, get the first and last element from a list

--- exercise7.py
+++ exercise7.py
@@ -2,4 +2,4 @@
 
 file_name = input("Enter the file name: ")
 separate_file = file_name.split(".")
-print("The file extension is: {}".format(separate_file[1]))
+print("The file extension is: {}".format(separate_file[len(separate_file) - 1]))
---
+++ exercise8.py
@@ -0,0 +1,6 @@
+# Get the first and last element from a list
+
+languages = ["Python", "C#", "PHP", "Java", "JavaScript"]
+
+print(languages[0])
+print(languages[len(languages) - 1])
